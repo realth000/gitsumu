9fac947ab0657c32e78115ee67cbb740eda2924a
realth000 <a598919335@gmail.com>
2024-02-01 15:04:42 +0800

    style(example): Format generated code in example
    [skip ci]

--- example/lib/example.g.dart
+++ example/lib/example.g.dart
@@ -1,32 +1,32 @@
 part of 'example.dart';
 
 // Compile environment
-const flutterVersion         = '3.16.5';
-const flutterChannel         = 'stable';
-const flutterFrameworkRevision  = '78666c8dc5';
+const flutterVersion = '3.16.5';
+const flutterChannel = 'stable';
+const flutterFrameworkRevision = '78666c8dc5';
 const flutterFrameworkTimestamp = '2023-12-19 16:14:14 -0800';
-const flutterEngineRevision  = '3f3e560236';
-const flutterDartVersion     = '3.2.3';
+const flutterEngineRevision = '3f3e560236';
+const flutterDartVersion = '3.2.3';
 const flutterDevToolsVersion = '2.28.4';
 
-const dartVersion            = '3.2.3 (stable)';
+const dartVersion = '3.2.3 (stable)';
 
 // Repo info
-const gitCommitTimeYear      = '2024';
-const gitCommitTimeMonth     = '01';
-const gitCommitTimeDay       = '22';
-const gitCommitTimeHour      = '03';
-const gitCommitTimeMinute    = '20';
-const gitCommitTimeSecond    = '18';
-const gitCommitTimeYMDHMS  = '2024-01-22 03:20:18';
-const gitCommitTimeTimezone  = '+0800';
-const gitCommitRevisionLong  = 'b9528509e1be69f38b962304b6a0fe068b04008f';
+const gitCommitTimeYear = '2024';
+const gitCommitTimeMonth = '01';
+const gitCommitTimeDay = '22';
+const gitCommitTimeHour = '03';
+const gitCommitTimeMinute = '20';
+const gitCommitTimeSecond = '18';
+const gitCommitTimeYMDHMS = '2024-01-22 03:20:18';
+const gitCommitTimeTimezone = '+0800';
+const gitCommitRevisionLong = 'b9528509e1be69f38b962304b6a0fe068b04008f';
 const gitCommitRevisionShort = 'b952850';
 
 // App info
-const appName        = 'example';
+const appName = 'example';
 const appDescription = 'A sample command-line application.';
-const appVersion     = '1.0.0';
+const appVersion = '1.0.0';
 
 // Custom info
 const myCommandResult = '''A command-line utility for Dart development.
@@ -57,7 +57,8 @@ Available commands:
 Run "dart help <command>" for more information about a command.
 See https://dart.dev/tools/dart-tool for detailed documentation.''';
 const myCommandResult2 = '''''';
-const myCommandFromStderr = '''git: 'abc' is not a git command. See 'git --help'.
+const myCommandFromStderr =
+    '''git: 'abc' is not a git command. See 'git --help'.
 
 The most similar command is
 	add''';
